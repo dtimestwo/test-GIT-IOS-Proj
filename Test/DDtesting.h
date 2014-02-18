7b76b43264300d7cf3f13fd40a789615e73fbf3d
Don <administrator@dons-MacBook-Pro.local>
2014-02-18 10:41:34 -0500

    added testing class

---
+++ Test/DDtesting.h
@@ -0,0 +1,13 @@
+//
+//  DDtesting.h
+//  Test
+//
+//  Created by Don on 2/18/14.
+//  Copyright (c) 2014 DSqared Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDtesting : NSObject
+
+@end
---
+++ Test/DDtesting.m
@@ -0,0 +1,13 @@
+//
+//  DDtesting.m
+//  Test
+//
+//  Created by Don on 2/18/14.
+//  Copyright (c) 2014 DSqared Tech. All rights reserved.
+//
+
+#import "DDtesting.h"
+
+@implementation DDtesting
+
+@end
